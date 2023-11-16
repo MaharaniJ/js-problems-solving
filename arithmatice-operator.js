e48c18c4b76c02933c0e96eecae04c49edd881ec
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-11-16 22:44:17 +0530

    reference-devstring.com

---
+++ arithmatice-operator.js
@@ -0,0 +1,8 @@
+var x = 10 + 15; // adding numbers
+var y = 2.5 + 3.4; // float number
+console.log(x, y);
+
+var a = 5, b = 10, c;
+c = a + b; // adding variables
+var d = (1 + 2 + 3 + 4 + 5) * c; // addition in expression
+console.log(d);
